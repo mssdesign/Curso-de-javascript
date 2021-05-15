8bb3a93d80ad87699615c0465a719cdcdae05465
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-05-15 13:34:59 -0300

    a1

---
+++ Aula 5/exercicio008.js
@@ -0,0 +1,7 @@
+// Verificar se deve ser multado
+var vel = 65.5
+console.log(`A velocidade do seu carrro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
