0d90445e458537253d2a1e49b7cac0197281a737
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-05-20 11:28:46 -0300

    Radar de multa

---
+++ Aula 6/exercicio008.js
@@ -0,0 +1,7 @@
+// Verificar se deve ser multado (SEM RELAÇÃO COM O OUTRO EXERCÍCIO)
+var vel = 65.5
+console.log(`A velocidade do seu carrro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
