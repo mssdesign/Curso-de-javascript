5a58ecacfcb65578ae30c80200d22dca05fc6ee4
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-05-26 11:56:41 -0300

    Aula 14

---
+++ Aula 14/ambiente.js
@@ -0,0 +1,23 @@
+// Estruturas de repetição
+
+
+/* Opção 1*/
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+
+/* Opção 2*/
+var c = 1
+do {       
+    console.log(c)
+    c++  
+} while (c <= 5);
+
+
+/* Opção 3*/
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
