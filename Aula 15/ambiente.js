303923e35fa0c2d5197434e1961c9d14ab330280
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-05-27 09:39:03 -0300

    Conceitos de array

---
+++ Aula 15/ambiente.js
@@ -0,0 +1,13 @@
+/*Conceitos básicos de array*/
+let num = [5, 8, 2, 9 ,3]
+
+/*Adicionando novo mais um número*/
+num[5]=7
+
+/*Adicionando novo mais um número no final sem especificar o final*/
+num.push(1)
+
+/*Para descobrir a quantidade de elementos dentro da variável array*/
+console.log(num.length)
+
+console.log(`Nosso vetor é o ${num}`)
