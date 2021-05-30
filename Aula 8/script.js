05869e74b7b25200d92f7e3370760b9c564c7584
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-05-30 12:08:29 -0300

    Aula 8

--- Aula 8/script.js
+++ Aula 8/script.js
@@ -2,17 +2,25 @@ const rock = document.querySelector('.rock');
 
 function eu() {
     alert('Olá, eu sou irock');
-    alert('Clique em mim para conversarmos');
-    setTimeout("alert('Wake up!');", 6000);
+    alert('Clique em mim para conversarmos');    
+    setInterval(sad, 10000);
 }
+
 
 function askname() {
     var usrName = prompt('Qual é o seu nome?',);
 
     if (usrName) {
         alert('Prazer em conhecer você,' + usrName + '!');
+        rock.classList.remove('sad')
         rock.classList.add('active')
     }    
+}
+
+function sad() {
+    rock.classList.remove('active')
+    rock.classList.add('sad')
+    window.alert('Onde está você?')
 }
 
 
