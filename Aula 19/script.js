f316be7d5931c14740d9aa6b5862e620c1e09d07
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-06-10 18:41:42 -0300

    Super contador

--- Aula 19/script.js
+++ Aula 19/script.js
@@ -1,8 +1,9 @@
-const inicio = document.getElementById('inicio');
-const fim = document.getElementById('fim');
-const passo = document.getElementById('passo');
-const contar = document.getElementById('contar');
-const console = document.getElementById('console');
+const inicio = document.getElementById('inicio')
+const fim = document.getElementById('fim')
+const passo = document.getElementById('passo')
+const contar = document.getElementById('contar')
+const console = document.getElementById('console')
+const tentar = document.querySelector('.tentar_n')
 
 contar.addEventListener('click', () => {
     var n_inicio = Number(inicio.value)
@@ -11,38 +12,40 @@ contar.addEventListener('click', () => {
     var res = Number(n_inicio)
 
     console.innerText = `Contando: `
-
-    if (n_inicio < n_fim) {
+    
+    if (n_inicio < n_fim && n_passo > 0) {
         somar()
-    } else if (n_inicio > n_fim) {
+    } else if (n_inicio > n_fim && n_passo != 0) {
         diminuir()
+    } else if (n_inicio == n_fim && n_passo != 0) {
+        alert('Defina um fim maior ou menor que o início!')
+        console.innerText = ` Não foi possível contar :/`
+    } else if (n_passo == 0) {
+        alert('Defina um passo maior que zero!')
+        console.innerText = ` Não foi possível contar :/`
     }
 
     function somar() {
         if (res < n_fim) {
-            console.innerText += ` ${res} `
+            console.innerText += ` ${res} 👉`
             res = (res + n_passo)                       
             somar()
-        }       
+        } else {
+            console.innerText += `🏆`
+        }        
     }
 
     function diminuir() {
         if (res > n_fim) {
-            console.innerText += ` ${res} `
+            console.innerText += ` ${res} 👉`
             res = (res - n_passo)            
             diminuir()
-        }         
+        } else {
+            console.innerText += `🏆`
+        }        
     }
 })
 
-// resolver caso n_passo 0
-// Aprender a debuggar
-// 👉
-
-
-
-
-
-
-
-
+function denovo() {
+    window.location.reload()
+}
