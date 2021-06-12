9be7dfcc0ae650edfff9e427e9d34fbd766dc536
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-06-12 16:35:13 -0300

    Super contador

--- Aula 19/script.js
+++ Aula 19/script.js
@@ -13,15 +13,15 @@ contar.addEventListener('click', () => {
 
     console.innerText = `Contando: `
     
-    if (n_inicio < n_fim && n_passo != 0) {
+    if (n_inicio < n_fim && n_passo != 0 && n_passo > 0) {
         somar()
-    } else if (n_inicio > n_fim && n_passo != 0) {
+    } else if (n_inicio > n_fim && n_passo != 0 && n_passo > 0) {
         diminuir()
-    } else if (n_inicio == n_fim && n_passo != 0) {
+    } else if (n_inicio == n_fim && n_passo != 0 && n_passo > 0) {
         alert('Defina um fim maior ou menor que o início!');
         console.innerText = ` Não foi possível contar :/`;
         tentar.style = "background-color: #FF0000";       
-    } else if (n_passo == 0) {
+    } else if (n_passo <= 0 || n_passo == 0) {
         alert('Defina um passo maior que zero!');
         console.innerText = ` Não foi possível contar :/`;
         tentar.style = "background-color: #FF0000";
