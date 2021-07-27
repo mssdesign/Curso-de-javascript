df8343d73a74e0628e86741cae6488cb66271609
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-07-27 19:37:22 -0300

    Coleção de Músicas

---
+++ Aula 23/script.js
@@ -0,0 +1,54 @@
+var recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+
+    if(prop !== "tracks" && value !== "") {
+  
+        records[id][prop] = value;
+  
+    } else if (value === "") {
+  
+        delete records[id][prop];
+  
+    } else if (prop === "tracks" && records[id].hasOwnProperty('tracks') === false) {
+  
+        records[id][prop] = [];
+        records[id]["tracks"].push(value);
+  
+    } else if (prop === "tracks" && value !== "") {
+  
+        records[id]["tracks"].push(value);
+  
+    }
+  
+    return records;
+  }
+
+  console.log(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"));
+  
+//   console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
+//   console.log(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"));
+//   console.log(updateRecords(recordCollection, 2548, "artist", ""));
+//   console.log(updateRecords(recordCollection, 1245, "tracks", "Addicted to Love"));
+//   console.log(updateRecords(recordCollection, 2548, "tracks", ""));
+//   console.log(updateRecords(recordCollection, 1245, "albumTitle", "Riptide"));
+
