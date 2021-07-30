94a8f310728e5afed9967528be074bcb33816957
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-07-30 10:10:09 -0300

    somar + recursividade

---
+++ Aula 23/sum.js
@@ -0,0 +1,14 @@
+var soma = 0;
+function sum(arr, n) {
+
+// Only change code below this line
+    if(n <= 0) {
+        console.log(soma);
+    } else {
+        soma += arr[n-1]
+        sum(arr, n-1)
+    }
+// Only change code above this line
+}
+
+sum([2, 3, 4, 5], 3)
