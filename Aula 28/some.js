63943a7d8e47629f9f914d3fc26ddb60cd327998
Matheus Soares <57039144+mssdesign@users.noreply.github.com>
2021-08-25 11:22:07 -0300

    Functional Programing

---
+++ Aula 28/some.js
@@ -0,0 +1,11 @@
+function checkPositive(arr) {
+    // Only change code below this line
+    return arr.some(function (a) {
+      return a > 0;
+    })
+  
+    // Only change code above this line
+  }
+  console.log(checkPositive([1, 2, 3, -4, 5]));
+
+  
